0c212cf365bd9df40f9fa574772a512a8e0758f8
Len <LenAnderson@users.noreply.github.com>
2016-04-28 14:44:44 +0200

    Larger storyboard images and related videos

--- youtube_mouseover_preview.user.js
+++ youtube_mouseover_preview.user.js
@@ -2,7 +2,7 @@
 // @name         YouTube - Mouseover Preview
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/YouTube-Mouseover-Preview/raw/master/youtube_mouseover_preview.user.js
-// @version      0.2
+// @version      1.0
 // @author       LenAnderson
 // @match        https://www.youtube.com/*
 // @grant        none
@@ -24,7 +24,7 @@
         mo.observe(document.body, {childList: true, subtree: true});
     }
     function initOn(base) {
-        [].forEach.call(base.querySelectorAll('.yt-lockup-thumbnail a[href^="/watch"]'), function(link) {
+        [].forEach.call(base.querySelectorAll('.yt-lockup-thumbnail a[href^="/watch"], .thumb-wrapper a[href^="/watch"'), function(link) {
             link.parentNode.addEventListener('mouseover', function() {
                 if (link.storyboard) return;
                 var spinner = document.createElement('div');
@@ -34,13 +34,13 @@
                 spinner.style.bottom = '0';
                 spinner.style.left = '0';
                 spinner.style.background = 'rgba(255,255,255,0.5)';
-                spinner.style.fontSize = '16px';
+                spinner.style.fontSize = '14px';
                 spinner.style.textAlign = 'center';
-                spinner.style.lineHeight = '7';
+                spinner.style.lineHeight = '2';
                 spinner.style.color = 'rgb(0,0,0)';
                 spinner.style.fontWeight = 'bold';
                 spinner.textContent = 'Loading Storyboard...';
-                var container = link.querySelector('.yt-thumb.video-thumb');
+                var container = link.querySelector('.yt-thumb.video-thumb, .yt-uix-simple-thumb-wrap.yt-uix-simple-thumb-related');
                 container.appendChild(spinner);
                 loadStoryboard(link).then(function(imgs) {
                     var frame = document.createElement('img');
@@ -54,7 +54,7 @@
             link.parentNode.addEventListener('mousemove', function(evt) {
                 if (!link.storyboard) return;
                 link.storyboard.then(function(imgs) {
-                    showFrame(link.querySelector('.yt-thumb.video-thumb'), evt.clientX, link.frame, imgs);
+                    showFrame(link.querySelector('.yt-thumb.video-thumb, .yt-uix-simple-thumb-wrap.yt-uix-simple-thumb-related'), evt.clientX, link.frame, imgs);
                 });
             });
             link.parentNode.addEventListener('mouseout', function(evt) {
@@ -71,7 +71,7 @@
     function showFrame(container, x, frame, imgs) {
         var totalFrames = 0;
         imgs.forEach(function(it) {
-            it.frames = it.height / 45 * 10;
+            it.frames = it.height / 90 * 5;
             totalFrames += it.frames;
         });
         x = x - container.getBoundingClientRect().left;
@@ -80,29 +80,29 @@
         var i = -1;
         var rows = 0;
         while (i+1 < imgs.length && lastFrame < frameIdx) {
-            rows = (lastFrame+1) / 10;
+            rows = (lastFrame+1) / 5;
             lastFrame += imgs[++i].frames;
         }
-        var w = imgs[i].width / 10;
+        var w = imgs[i].width / 5;
         var iw;
         var ih;
         var fw;
         var fh;
-        if (container.offsetWidth / container.offsetHeight > w / 45) {
+        if (container.offsetWidth / container.offsetHeight > w / 90) {
             iw = Math.round(container.offsetWidth / w * imgs[i].width);
             ih = Math.round(container.offsetWidth / w * imgs[i].height);
             fw = container.offsetWidth;
-            fh = Math.round(container.offsetWidth / w * 45);
+            fh = Math.round(container.offsetWidth / w * 90);
         } else {
-            iw = Math.round(container.offsetHeight / 45 * imgs[i].width);
-            ih = Math.round(container.offsetHeight / 45 * imgs[i].height);
-            fw = Math.round(container.offsetHeight / 45 * w);
+            iw = Math.round(container.offsetHeight / 90 * imgs[i].width);
+            ih = Math.round(container.offsetHeight / 90 * imgs[i].height);
+            fw = Math.round(container.offsetHeight / 90 * w);
             fh = container.offsetHeight;
         }
         frame.src = imgs[i].src;
         frame.style.width = iw + 'px';
-        var row = Math.floor(frameIdx / 10) - rows;
-        var col = frameIdx % 10;
+        var row = Math.floor(frameIdx / 5) - rows;
+        var col = frameIdx % 5;
         frame.style.top = -fh * row + 'px';
         frame.style.left = -fw * col + 'px';
         frame.style.opacity = 1;
@@ -118,10 +118,10 @@
             xhr.addEventListener('load', function() {
                 var reg = /"storyboard_spec":\s*"(.*?)"/g;
                 var spec = reg.exec(xhr.responseText)[1];
-                reg = /(http.*?)\|.*?#M\$M#(.*?)\|/g;
+                reg = /(http.*?)\|.*?#M\$M#(.*?)\|.*?#M\$M#(.*?)$/g;
                 spec = reg.exec(spec);
-                var http = spec[1].replace(/\\/g, '').replace('$L', '1');
-                var sigh = spec[2];
+                var http = spec[1].replace(/\\/g, '').replace('$L', '2');
+                var sigh = spec[3];
                 http += '?sigh='+sigh;
                 reg = /"length_seconds":\s*"(\d+)"/;
                 var length = reg.exec(xhr.responseText)[1];
